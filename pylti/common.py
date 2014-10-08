aca67adf46ad999f4c3b30f99fa567669c0f8e4b
Ivica Ceraj <iceraj@gmail.com>
2014-10-08 12:37:09 -0400

    Adding authentication

---
+++ pylti/common.py
@@ -0,0 +1,187 @@
+from __future__ import absolute_import
+"""
+Classes to handle oauth portion of LTI
+"""
+# pylint: disable=C0103
+import logging
+# pylint: disable=C0103
+from functools import wraps
+
+from flask import Flask, session, request
+
+import oauth.oauth as oauth
+
+log = logging.getLogger('pylti.common')
+
+class LTIOAuthDataStore(oauth.OAuthDataStore):
+    """
+    Largely taken from reference implementation
+    for app engine at https://code.google.com/p/ims-dev/
+    """
+
+    def __init__(self,consumers):
+        """
+        Create OAuth store
+        """
+        oauth.OAuthDataStore.__init__(self)
+        self.consumers = consumers
+
+    def lookup_consumer(self, key):
+        """
+        Search through keys
+        """
+        if not self.consumers:
+            log.critical(("No consumers defined in settings."
+                          "Have you created a configuration file?"))
+            return None
+
+        consumer = self.consumers.get(key)
+        if not consumer:
+            log.info("Did not find consumer, using key: %s ", key)
+            return None
+
+        secret = consumer.get('secret', None)
+        if not secret:
+            log.critical(('Consumer %s, is missing secret'
+                          'in settings file, and needs correction.'), key)
+            return None
+        return oauth.OAuthConsumer(key, secret)
+
+    def lookup_token(self, oauth_consumer, token_type, token):
+        """We don't do request_tokens"""
+        # pylint: disable=W0613
+        return oauth.OAuthToken(None, None)  # pragma: no cover
+
+    def lookup_nonce(self, oauth_consumer, oauth_token, nonce):
+        """Trust all nonces"""
+        return None  # pragma: no cover
+
+    def fetch_request_token(self, oauth_consumer, oauth_callback):
+        """We don't do request_tokens"""
+        return None  # pragma: no cover
+
+    def fetch_access_token(self, oauth_consumer, oauth_token, oauth_verifier):
+        """We don't do request_tokens"""
+        return None  # pragma: no cover
+
+    def authorize_request_token(self, oauth_token, user):
+        """We don't do request_tokens"""
+        return None  # pragma: no cover
+
+"""
+Utility support functions
+"""
+
+class LTIException(Exception):
+    """
+    Custom LTI exception for proper handling
+    of LTI specific errors
+    """
+    pass
+
+class LTINotInSessionException(LTIException):
+    """
+    Custom LTI exception for proper handling
+    of LTI specific errors
+    """
+    pass
+
+class LTIRoleException(LTIException):
+    """
+    Exception class for when LTI user doesn't have the
+    right role.
+    """
+    pass
+
+"""
+Decorators to handle LTI session management,
+authentication, etc.
+"""
+
+LTI_PROPERTY_LIST = [
+    'oauth_consumer_key',
+    'launch_presentation_return_url',
+    'user_id',
+    'oauth_nonce',
+    'context_label',
+    'context_id',
+    'resource_link_title',
+    'resource_link_id',
+    'lis_person_contact_email_primary',
+    'lis_person_contact_emailprimary',
+    'lis_person_name_full',
+    'lis_person_name_family',
+    'lis_person_name_given',
+    'lis_result_sourcedid',
+    'launch_type',
+    'lti_message',
+    'lti_version',
+    'roles',
+]
+
+LTI_STAFF_ROLES = ['Instructor', 'Administrator', ]
+
+LTI_SESSION_KEY = 'lti_authenticated'
+
+
+def verify_request_common(consumers,url,method,headers,params):
+    oauth_store = LTIOAuthDataStore(consumers)
+    oauth_server = oauth.OAuthServer(oauth_store)
+    oauth_server.add_signature_method(
+        oauth.OAuthSignatureMethod_PLAINTEXT())
+    oauth_server.add_signature_method(
+        oauth.OAuthSignatureMethod_HMAC_SHA1())
+
+    # Check header for SSL before selecting the url
+    if headers.get('X-Forwarded-Proto', 'http') == 'https':
+        url = url.replace('http', 'https', 1)
+
+    oauth_request = oauth.OAuthRequest.from_request(
+        method,
+        url,
+        headers=dict(headers),
+        parameters=params
+    )
+
+    if not oauth_request:
+        log.info('Received non oauth request on oauth protected page')
+        raise LTIException('This page requires a valid oauth session '
+                           'or request')
+    try:
+        consumer = oauth_server._get_consumer(oauth_request)
+        oauth_server._check_signature(oauth_request, consumer, None)
+    except oauth.OAuthError as err:
+        # Rethrow our own for nice error handling (don't print
+        # error message as it will contain the key
+        log.info(err.message)
+        raise LTIException("OAuth error: Please check your key and secret")
+
+    return True
+
+def lti_staff_required(func):
+    """
+    Decorator to make sure that person is a
+    member of one of the course staff roles
+    before allowing them to the view. Requires that
+    lti_authentication has occurred
+    """
+    @wraps(func)
+    def decorator(*args, **kwargs):
+        """
+        Check session['role'] against known list of course staff
+        roles and raise if it isn't in that set.
+        """
+        log.debug(session)
+        role = session.get('roles', None)
+        if not role:
+            raise LTIRoleException(
+                'User does not have a role. One is required'
+            )
+        if role not in LTI_STAFF_ROLES:
+            raise LTIRoleException(
+                'You are not in a staff level role. Access is restricted '
+                'to course staff.'
+            )
+        return func(*args, **kwargs)
+
+    return decorator
--- pylti/flask.py
+++ pylti/flask.py
@@ -1,21 +1,122 @@
-from functools import wraps
+from __future__ import absolute_import
+from flask import Flask, session, request
+from functools import wraps, partial
+from .common import LTI_SESSION_KEY, LTI_PROPERTY_LIST, LTI_STAFF_ROLES, verify_request_common, LTIException,LTINotInSessionException
+
+import logging
+log = logging.getLogger('pylti.flask')
+
 
 # request = 'any' || 'initial' || 'session'
 
-def lti(*args,**kwargs):
-    def _lti(f):
+
+class LTIVerificationFailedException(Exception):
+    pass
+
+class LTI():
+    def __init__(self, lti_args, lti_kwargs):
+        self.lti_args = lti_args
+        self.lti_kwargs = lti_kwargs
+
+        self.nickname = 'nickname'
+
+    def verify(self):
+        log.debug('verify request={}'.format( self.lti_kwargs.get('request')))
+        if ( self.lti_kwargs.get('request') == 'session' ):
+            self.verify_session()
+        elif ( self.lti_kwargs.get('request') == 'initial'):
+            self.verify_request()
+        elif ( self.lti_kwargs.get('request') == 'any'):
+            self.verify_any()
+        else:
+            raise LTIException()
+
+    def verify_any(self):
+        log.debug('verify_any enter')
+        try:
+            self.verify_session()
+        except LTINotInSessionException as e:
+            self.verify_request()
+
+    def verify_session(self):
+        if not session.get(LTI_SESSION_KEY, False):
+            log.debug('verify_session failed')
+            raise LTINotInSessionException()
+
+    def _consumers(self):
+        app_config = self.lti_kwargs['app'].config
+        config = app_config.get('PYLTI_CONFIG',dict())
+        consumers = config.get('consumers',dict())
+        return consumers
+
+    def verify_request(self):
+        if request.method == 'POST':
+            params = request.form.to_dict()
+        else:
+            params = request.args.to_dict()
+        log.debug(params)
+
+        log.debug('verify_request?')
+        if verify_request_common(self._consumers(),request.url, request.method, request.headers, params):
+            log.debug('verify_request success')
+
+            # All good to go, store all of the LTI params into a
+            # session dict for use in views
+            for prop in LTI_PROPERTY_LIST:
+                if params.get(prop, None):
+                    session[prop] = params[prop]
+
+            # Set logged in session key
+            session[LTI_SESSION_KEY] = True
+            return True
+        else:
+            log.debug('verify_request failed')
+            for prop in LTI_PROPERTY_LIST:
+                if session.get(prop, None):
+                    del session[prop]
+
+            session[LTI_SESSION_KEY] = False
+            raise LTIException
+
+    def post_grade(self, grade):
+        print "Grade {}".format(grade)
+        return False
+
+    def close_session(self):
+        for prop in LTI_PROPERTY_LIST:
+            if session.get(prop, None):
+                del session[prop]
+        session[LTI_SESSION_KEY] = False
+
+
+
+def lti(*lti_args, **lti_kwargs):
+    def _lti(f, lti_args=[], lti_kwargs=dict()):
         @wraps(f)
         def wrapper(*args, **kwargs):
-            print "request kind {} ".format(request)
-            ret = f(*args,**kwargs)
-            return ret
+            try:
+                the_lti = LTI(lti_args,lti_kwargs)
+                the_lti.verify()
+                kwargs['lti'] = the_lti
+                ret = f(*args, **kwargs)
+                return ret
+            except LTIException as e:
+                error = lti_kwargs.get('error')
+                exception = dict()
+                exception['exception']=e
+                exception['kwargs']=kwargs
+                exception['args']=args
+                ret = error(exception=exception)
+                return ret
         return wrapper
-    if len(args) == 1 and callable(args[0]):
+
+    if len(lti_args) == 1 and callable(lti_args[0]):
         # No arguments, this is the decorator
         # Set default values for the arguments
-        enter_string = 'entering'
-        exit_string = 'exiting'
-        return _lti(args[0])
+        ret = _lti(lti_args[0])
     else:
-        request = args
-        return _lti
+        ret = partial(_lti, *lti_args, lti_args=lti_args, lti_kwargs=lti_kwargs)
+
+    return ret
+
+

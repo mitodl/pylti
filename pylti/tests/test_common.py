6012933924852e22596b7782cd5d95426aac26ee
Ivica Ceraj <ceraj@mit.edu>
2014-10-24 14:35:58 -0400

    doc_tests

--- pylti/tests/test_common.py
+++ pylti/tests/test_common.py
@@ -25,11 +25,38 @@ import oauthlib.oauth1
 
 
 class TestCommon(unittest.TestCase):
+    expected_response = """<?xml version="1.0" encoding="UTF-8"?>
+<imsx_POXEnvelopeResponse xmlns = "http://www.imsglobal.org/services/ltiv1p1\
+/xsd/imsoms_v1p0">
+    <imsx_POXHeader>
+        <imsx_POXResponseHeaderInfo>
+            <imsx_version>V1.0</imsx_version>
+            <imsx_messageIdentifier>edX_fix</imsx_messageIdentifier>
+            <imsx_statusInfo>
+                <imsx_codeMajor>success</imsx_codeMajor>
+                <imsx_severity>status</imsx_severity>
+                <imsx_description>Score for StarX/StarX_DEMO/201X_StarX:\
+edge.edx.org-i4x-StarX-StarX_DEMO-lti-40559041895b4065b2818c23b9cd9da8\
+:18b71d3c46cb4dbe66a7c950d88e78ec is now 0.0</imsx_description>
+                <imsx_messageRefIdentifier>
+                </imsx_messageRefIdentifier>
+            </imsx_statusInfo>
+        </imsx_POXResponseHeaderInfo>
+    </imsx_POXHeader>
+    <imsx_POXBody><replaceResultResponse/></imsx_POXBody>
+</imsx_POXEnvelopeResponse>
+        """
 
     def test_hasversion(self):
+        """
+        Tests that pylti VERSION is valid. http://semver.org/
+        """
         self.assertRegexpMatches(pylti.VERSION, "[0-9]+\\.[0-9]+\\.[0-9]+")
 
     def test_ltioauthdatastore(self):
+        """
+        Tests that LTIOAuthDataStore works
+        """
         consumers = {
             "key1": {"secret": "secret1"},
             "key2": {"secret": "secret2"},
@@ -44,10 +71,141 @@ class TestCommon(unittest.TestCase):
         self.assertIsNone(store.lookup_consumer("keyNS"))
 
     def test_ltioauthdatastore_no_consumers(self):
+        """
+        If consumers are not given it there are no consumer to return.
+
+        """
         store = LTIOAuthDataStore(None)
         self.assertIsNone(store.lookup_consumer("key1"))
 
+    def test_verify_request_common(self):
+        """
+        verify_request_common succeeds on valid request
+        """
+        headers = dict()
+        consumers, method, url, verify_params, params = \
+            self.generate_oauth_request()
+        ret = verify_request_common(consumers, url, method,
+                                    headers, verify_params)
+        self.assertTrue(ret)
+
+    def test_verify_request_common_via_proxy(self):
+        """
+        verify_request_common succeeds on valid request via proxy
+        """
+        headers = dict()
+        headers['X-Forwarded-Proto'] = 'https'
+        orig_url = 'https://localhost:5000/?'
+        consumers, method, url, verify_params, params = \
+            self.generate_oauth_request(url_to_sign=orig_url)
+        ret = verify_request_common(consumers, url, method,
+                                    headers, verify_params)
+        self.assertTrue(ret)
+
+    def test_verify_request_common_no_auth_fields(self):
+        """
+        verify_request_common fails on missing authentication
+        """
+        headers = dict()
+        consumers, method, url, verify_params, params = \
+            self.generate_oauth_request()
+        ret = False
+        try:
+            ret = verify_request_common(consumers, url, method,
+                                        headers, params)
+        except LTIException:
+            self.assertTrue(True)
+        self.assertFalse(ret)
+
+    def test_verify_request_common_no_params(self):
+        """
+        verify_request_common fails on missing parameters
+        """
+        consumers = {
+            "__consumer_key__": {"secret": "__lti_secret__"}
+        }
+        url = 'http://localhost:5000/?'
+        method = 'GET'
+        headers = dict()
+        params = dict()
+        ret = False
+        try:
+            ret = verify_request_common(consumers, url, method,
+                                        headers, params)
+        except LTIException:
+            self.assertTrue(True)
+        self.assertFalse(ret)
+
+    @httpretty.activate
+    def test_post_response_invalid_xml(self):
+        """
+        Test post message with invalid XML response
+        """
+        uri = (u'https://edge.edx.org/courses/MITx/ODL_ENG/2014_T1/xblock/'
+               u'i4x:;_;_MITx;_ODL_ENG;_lti;_94173d3e79d145fd8ec2e83f15836ac8/'
+               u'handler_noauth/grade_handler')
+
+        def request_callback(request, cburi, headers):
+            return 200, headers, "success"
+
+        httpretty.register_uri(httpretty.POST, uri, body=request_callback)
+        consumers = {
+            "__consumer_key__": {"secret": "__lti_secret__"}
+        }
+        body = '<xml></xml>'
+        ret = post_message(consumers, "__consumer_key__", uri, body)
+        self.assertFalse(ret)
+
+    @httpretty.activate
+    def test_post_response_valid_xml(self):
+        """
+        Test post grade with valid XML response
+        """
+        uri = 'https://localhost:8000/dev_stack'
+
+        def request_callback(request, cburi, headers):
+            return 200, headers, self.expected_response
+
+        httpretty.register_uri(httpretty.POST, uri, body=request_callback)
+        consumers = {
+            "__consumer_key__": {"secret": "__lti_secret__"}
+        }
+        body = generate_request_xml('message_identifier_id', 'operation',
+                                    'lis_result_sourcedid', '1.0')
+        ret = post_message(consumers, "__consumer_key__", uri, body)
+        self.assertTrue(ret)
+
+    def test_generate_xml(self):
+        """
+        Generated post XML is valid
+        """
+        xml = generate_request_xml('message_identifier_id', 'operation',
+                                   'lis_result_sourcedid', 'score')
+        self.assertEqual(xml, """<?xml version='1.0' encoding='utf-8'?>
+<imsx_POXEnvelopeRequest xmlns="http://www.imsglobal.org/services/ltiv1p1/xsd/\
+imsoms_v1p0"><imsx_POXHeader><imsx_POXRequestHeaderInfo><imsx_version>V1.0\
+</imsx_version><imsx_messageIdentifier>message_identifier_id\
+</imsx_messageIdentifier></imsx_POXRequestHeaderInfo></imsx_POXHeader>\
+<imsx_POXBody><operationRequest><resultRecord><sourcedGUID><sourcedId>\
+lis_result_sourcedid</sourcedId></sourcedGUID><result><resultScore>\
+<language>en</language><textString>score</textString></resultScore>\
+</result></resultRecord></operationRequest></imsx_POXBody>\
+</imsx_POXEnvelopeRequest>""")
+        xml = generate_request_xml('message_identifier_id', 'operation',
+                                   'lis_result_sourcedid', None)
+        self.assertEqual(xml, """<?xml version='1.0' encoding='utf-8'?>
+<imsx_POXEnvelopeRequest xmlns="http://www.imsglobal.org/services/ltiv1p1/xsd/\
+imsoms_v1p0"><imsx_POXHeader><imsx_POXRequestHeaderInfo><imsx_version>V1.0\
+</imsx_version><imsx_messageIdentifier>message_identifier_id\
+</imsx_messageIdentifier></imsx_POXRequestHeaderInfo></imsx_POXHeader>\
+<imsx_POXBody><operationRequest><resultRecord><sourcedGUID><sourcedId>\
+lis_result_sourcedid</sourcedId></sourcedGUID></resultRecord></operationRequest>\
+</imsx_POXBody></imsx_POXEnvelopeRequest>""")
+
     def generate_oauth_request(self, url_to_sign=None):
+        """
+        This code generated valid LTI 1.0 basic-lti-launch-request request
+        """
         consumers = {
             "__consumer_key__": {"secret": "__lti_secret__"}
         }
@@ -88,89 +246,3 @@ class TestCommon(unittest.TestCase):
         for k, v in qs.iteritems():
             verify_params[k] = v[0]
         return consumers, method, url, verify_params, params
-
-    def test_verify_request_common(self):
-        headers = dict()
-        consumers, method, url, verify_params, params = \
-            self.generate_oauth_request()
-        ret = verify_request_common(consumers, url, method,
-                                    headers, verify_params)
-        self.assertTrue(ret)
-
-    def test_verify_request_common_via_proxy(self):
-        headers = dict()
-        headers['X-Forwarded-Proto'] = 'https'
-        orig_url = 'https://localhost:5000/?'
-        consumers, method, url, verify_params, params = \
-            self.generate_oauth_request(url_to_sign=orig_url)
-        ret = verify_request_common(consumers, url, method,
-                                    headers, verify_params)
-        self.assertTrue(ret)
-
-    def test_verify_request_common_no_auth_fields(self):
-        headers = dict()
-        consumers, method, url, verify_params, params = \
-            self.generate_oauth_request()
-        ret = False
-        try:
-            ret = verify_request_common(consumers, url, method,
-                                        headers, params)
-        except LTIException:
-            self.assertTrue(True)
-        self.assertFalse(ret)
-
-    def test_verify_request_common_no_params(self):
-        consumers = {
-            "__consumer_key__": {"secret": "__lti_secret__"}
-        }
-        url = 'http://localhost:5000/?'
-        method = 'GET'
-        headers = dict()
-        params = dict()
-        ret = False
-        try:
-            ret = verify_request_common(consumers, url, method,
-                                        headers, params)
-        except LTIException:
-            self.assertTrue(True)
-        self.assertFalse(ret)
-
-    @httpretty.activate
-    def test_post_response(self):
-        uri = (u'https://edge.edx.org/courses/MITx/ODL_ENG/2014_T1/xblock/'
-               u'i4x:;_;_MITx;_ODL_ENG;_lti;_94173d3e79d145fd8ec2e83f15836ac8/'
-               u'handler_noauth/grade_handler')
-
-        def request_callback(request, cburi, headers):
-            return 200, headers, "success"
-
-        httpretty.register_uri(httpretty.POST, uri, body=request_callback)
-        consumers = {
-            "__consumer_key__": {"secret": "__lti_secret__"}
-        }
-        body = '<xml></xml>'
-        post_message(consumers, "__consumer_key__", uri, body)
-
-    def test_generate_xml(self):
-        xml = generate_request_xml('message_identifier_id', 'operation',
-                                   'lis_result_sourcedid', 'score')
-        self.assertEqual(xml, """<?xml version='1.0' encoding='utf-8'?>
-<imsx_POXEnvelopeRequest xmlns="http://www.imsglobal.org/services/ltiv1p1/xsd/\
-imsoms_v1p0"><imsx_POXHeader><imsx_POXRequestHeaderInfo><imsx_version>V1.0\
-</imsx_version><imsx_messageIdentifier>message_identifier_id\
-</imsx_messageIdentifier></imsx_POXRequestHeaderInfo></imsx_POXHeader>\
-<imsx_POXBody><operationRequest><resultRecord><sourcedGUID><sourcedId>\
-lis_result_sourcedid</sourcedId></sourcedGUID><result><resultScore>\
-<language>en</language><textString>score</textString></resultScore>\
-</result></resultRecord></operationRequest></imsx_POXBody>\
-</imsx_POXEnvelopeRequest>""")
-        xml = generate_request_xml('message_identifier_id', 'operation',
-                                   'lis_result_sourcedid', None)
-        self.assertEqual(xml, """<?xml version='1.0' encoding='utf-8'?>
-<imsx_POXEnvelopeRequest xmlns="http://www.imsglobal.org/services/ltiv1p1/xsd/\
-imsoms_v1p0"><imsx_POXHeader><imsx_POXRequestHeaderInfo><imsx_version>V1.0\
-</imsx_version><imsx_messageIdentifier>message_identifier_id\
-</imsx_messageIdentifier></imsx_POXRequestHeaderInfo></imsx_POXHeader>\
-<imsx_POXBody><operationRequest><resultRecord><sourcedGUID><sourcedId>\
-lis_result_sourcedid</sourcedId></sourcedGUID></resultRecord></operationRequest>\
-</imsx_POXBody></imsx_POXEnvelopeRequest>""")
--- pylti/tests/test_flask.py
+++ pylti/tests/test_flask.py
@@ -14,10 +14,16 @@ from pylti.tests.test_flask_app import app_exception, app
 
 
 class TestFlask(unittest.TestCase):
+    """
+    Consumers
+    """
     consumers = {
         "__consumer_key__": {"secret": "__lti_secret__"}
     }
 
+    """
+    Valid XML response from LTI 1.0 consumer
+    """
     expected_response = """<?xml version="1.0" encoding="UTF-8"?>
 <imsx_POXEnvelopeResponse xmlns = "http://www.imsglobal.org/services/ltiv1p1\
 /xsd/imsoms_v1p0">
@@ -41,6 +47,9 @@ edge.edx.org-i4x-StarX-StarX_DEMO-lti-40559041895b4065b2818c23b9cd9da8\
         """
 
     def setUp(self):
+        """
+        Setting up app config
+        """
         app.config['TESTING'] = True
         app.config['SERVER_NAME'] = 'localhost'
         app.config['WTF_CSRF_ENABLED'] = False
@@ -55,15 +64,30 @@ edge.edx.org-i4x-StarX-StarX_DEMO-lti-40559041895b4065b2818c23b9cd9da8\
         app_exception.reset()
 
     def get_exception(self):
+        """
+        Returns exception raised by PyLTI
+        :return: exception
+        """
         return app_exception.get()
 
     def has_exception(self):
+        """
+        Check if PyLTI raised exception
+        :return: is exception raised
+        """
         return app_exception.get() is not None
 
     def get_exception_as_string(self):
+        """
+        Return text of the exception raised by LTI
+        :return: text
+        """
         return "{}".format(self.get_exception())
 
     def test_access_to_oauth_resource_unknown_protection(self):
+        """
+        Invalid LTI request scope
+        """
         self.app.get('/unknown_protection')
         self.assertTrue(self.has_exception())
         self.assertIsInstance(self.get_exception(), LTIException)
@@ -71,6 +95,9 @@ edge.edx.org-i4x-StarX-StarX_DEMO-lti-40559041895b4065b2818c23b9cd9da8\
                          'Unknown request type')
 
     def test_access_to_oauth_resource_without_authorization_any(self):
+        """
+        Accessing LTI without establishing session
+        """
         self.app.get('/any')
         self.assertTrue(self.has_exception())
         self.assertIsInstance(self.get_exception(), LTIException)
@@ -78,6 +105,9 @@ edge.edx.org-i4x-StarX-StarX_DEMO-lti-40559041895b4065b2818c23b9cd9da8\
                          'This page requires a valid oauth session or request')
 
     def test_access_to_oauth_resource_without_authorization_session(self):
+        """
+        Accessing LTI session scope before session established
+        """
         self.app.get('/session')
         self.assertTrue(self.has_exception())
         self.assertIsInstance(self.get_exception(), LTIException)
@@ -85,6 +115,9 @@ edge.edx.org-i4x-StarX-StarX_DEMO-lti-40559041895b4065b2818c23b9cd9da8\
                          'Session expired or unavailable')
 
     def test_access_to_oauth_resource_without_authorization_initial_get(self):
+        """
+        Accessing LTI without basic-lti-launch-request parameters as GET
+        """
         self.app.get('/initial')
         self.assertTrue(self.has_exception())
         self.assertIsInstance(self.get_exception(), LTIException)
@@ -92,6 +125,9 @@ edge.edx.org-i4x-StarX-StarX_DEMO-lti-40559041895b4065b2818c23b9cd9da8\
                          'This page requires a valid oauth session or request')
 
     def test_access_to_oauth_resource_without_authorization_initial_post(self):
+        """
+        Accessing LTI without basic-lti-launch-request parameters as POST
+        """
         self.app.post('/initial')
         self.assertTrue(self.has_exception())
         self.assertIsInstance(self.get_exception(), LTIException)
@@ -99,11 +135,18 @@ edge.edx.org-i4x-StarX-StarX_DEMO-lti-40559041895b4065b2818c23b9cd9da8\
                          'This page requires a valid oauth session or request')
 
     def test_access_to_oauth_resource_in_session(self):
+        """
+        Accessing LTI after session established
+        """
         self.app.get('/setup_session')
         self.app.get('/session')
         self.assertFalse(self.has_exception())
 
     def test_access_to_oauth_resource_in_session_with_close(self):
+        """
+        Accessing LTI after session closed
+        """
+
         self.app.get('/setup_session')
         self.app.get('/session')
         self.assertFalse(self.has_exception())
@@ -119,6 +162,9 @@ edge.edx.org-i4x-StarX-StarX_DEMO-lti-40559041895b4065b2818c23b9cd9da8\
         self.assertFalse(self.has_exception())
 
     def test_access_to_oauth_resource_staff_only_as_student(self):
+        """
+        Deny access if user not in role
+        """
         consumers = self.consumers
         url = 'http://localhost/initial_staff?'
         new_url = self.generate_launch_request(consumers, url,
@@ -127,6 +173,9 @@ edge.edx.org-i4x-StarX-StarX_DEMO-lti-40559041895b4065b2818c23b9cd9da8\
         self.assertTrue(self.has_exception())
 
     def test_access_to_oauth_resource_staff_only_as_administrator(self):
+        """
+        Allow access if user in role
+        """
         consumers = self.consumers
         url = 'http://localhost/initial_staff?'
         new_url = self.generate_launch_request(consumers, url,
@@ -135,6 +184,9 @@ edge.edx.org-i4x-StarX-StarX_DEMO-lti-40559041895b4065b2818c23b9cd9da8\
         self.assertFalse(self.has_exception())
 
     def test_access_to_oauth_resource_staff_only_as_unknown_role(self):
+        """
+        Deny access if role not defined
+        """
         consumers = self.consumers
         url = 'http://localhost/initial_unknown?'
         new_url = self.generate_launch_request(consumers, url,
@@ -145,6 +197,14 @@ edge.edx.org-i4x-StarX-StarX_DEMO-lti-40559041895b4065b2818c23b9cd9da8\
     def generate_launch_request(self, consumers, url,
                                 lit_outcome_service_url=None,
                                 roles=u'Instructor'):
+        """
+        Generate valid basic-lti-launch-request request with options
+        :param consumers: consumer map
+        :param url: URL to sign
+        :param lit_outcome_service_url: LTI callback
+        :param roles: LTI role
+        :return: signed request
+        """
         params = {'resource_link_id': u'edge.edx.org-i4x-MITx-ODL_ENG-lti-'
                                       u'94173d3e79d145fd8ec2e83f15836ac8',
                   'user_id': u'008437924c9852377e8994829aaac7a1',
@@ -182,12 +242,19 @@ edge.edx.org-i4x-StarX-StarX_DEMO-lti-40559041895b4065b2818c23b9cd9da8\
         return new_url
 
     def test_access_to_oauth_resource_any(self):
+        """
+        Test access to LTI protected resources
+        """
         url = 'http://localhost/any?'
         new_url = self.generate_launch_request(self.consumers, url)
         self.app.get(new_url)
         self.assertFalse(self.has_exception())
 
     def test_access_to_oauth_resource_invalid(self):
+        """
+        Deny access to LTI protected resources
+        on man in the middle attach
+        """
         url = 'http://localhost/initial?'
         new_url = self.generate_launch_request(self.consumers, url)
         self.app.get("{}&FAIL=TRUE".format(new_url))
@@ -197,6 +264,9 @@ edge.edx.org-i4x-StarX-StarX_DEMO-lti-40559041895b4065b2818c23b9cd9da8\
                          'OAuth error: Please check your key and secret')
 
     def test_access_to_oauth_resource_invalid_after_session_setup(self):
+        """
+        Remove browser session on man in the middle attach
+        """
         self.app.get('/setup_session')
         self.app.get('/session')
         self.assertFalse(self.has_exception())
@@ -211,6 +281,9 @@ edge.edx.org-i4x-StarX-StarX_DEMO-lti-40559041895b4065b2818c23b9cd9da8\
 
     @httpretty.activate
     def test_access_to_oauth_resource_post_grade(self):
+        """
+        Check post_grade functionality
+        """
         uri = (u'https://edge.edx.org/courses/MITx/ODL_ENG/2014_T1/xblock/'
                u'i4x:;_;_MITx;_ODL_ENG;_lti;'
                u'_94173d3e79d145fd8ec2e83f15836ac8/handler_noauth'
@@ -234,7 +307,10 @@ edge.edx.org-i4x-StarX-StarX_DEMO-lti-40559041895b4065b2818c23b9cd9da8\
         self.assertEqual(ret.data, "grade=False")
 
     @httpretty.activate
-    def test_access_to_oauth_resource_post_grade(self):
+    def test_access_to_oauth_resource_post_grade_fail(self):
+        """
+        Check post_grade functionality fails on invalid response
+        """
         uri = (u'https://edge.edx.org/courses/MITx/ODL_ENG/2014_T1/xblock/'
                u'i4x:;_;_MITx;_ODL_ENG;_lti;'
                u'_94173d3e79d145fd8ec2e83f15836ac8/handler_noauth'
@@ -256,6 +332,9 @@ edge.edx.org-i4x-StarX-StarX_DEMO-lti-40559041895b4065b2818c23b9cd9da8\
 
     @httpretty.activate
     def test_access_to_oauth_resource_post_grade_fix_url(self):
+        """
+        Make sure URL remap works for edX vagrant stack
+        """
         uri = 'https://localhost:8000/dev_stack'
 
         def request_callback(request, cburi, headers):

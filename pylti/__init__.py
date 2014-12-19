1b1dbe4f546e87fea615ed9a96f81368507fb853
Peter Pinch <pdpinch@mit.edu>
2014-12-19 13:28:40 -0500

    Bumping version to 0.3.0

--- pylti/__init__.py
+++ pylti/__init__.py
@@ -3,4 +3,4 @@
 PyLTI is module that implements IMS LTI in python
 The API uses decorators to wrap function with LTI functionality.
 """
-VERSION = "0.2.0"  # pragma: no cover
+VERSION = "0.3.0"  # pragma: no cover

63ef1ef2290ab72efa42064a1d13793782b39be9
Caio Vinicius Marques Teixeira <caio.vinicius.teixeira@usp.br>
2015-05-22 01:33:48 -0300

    Little PEP-8 fix

--- pylti/common.py
+++ pylti/common.py
@@ -40,7 +40,7 @@ LTI_ROLES = {
     u'instructor': [u'Instructor', ],
     u'administrator': [u'Administrator', ],
     u'student': [u'Student', u'Learner', ],
-    u'any': [u'Administrator', u'Instructor', u'Student', u'Learner',],
+    u'any': [u'Administrator', u'Instructor', u'Student', u'Learner', ],
 }
 
 LTI_SESSION_KEY = u'lti_authenticated'

9946036fb773827dd911eac2311c9b410af03051
Giovanni Di Milia <gdimilia@mit.edu>
2017-10-24 17:14:10 -0400

    Changed the version variable in __init__

--- pylti/__init__.py
+++ pylti/__init__.py
@@ -3,4 +3,4 @@
 PyLTI is module that implements IMS LTI in python
 The API uses decorators to wrap function with LTI functionality.
 """
-VERSION = "0.5.0"  # pragma: no cover
+__version__ = "0.5.0"  # pragma: no cover
